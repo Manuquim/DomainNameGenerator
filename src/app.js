29e483e2c09aad542e5e72ec6a53571fe0ea79d0
Manuquim <manuelcabreraosorio11@gmail.com>
2022-11-20 12:44:22 +0000

    hecho 20-11-11 13-44

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+  let aux = "";
+  for (let index1 in pronoun) {
+    for (let index2 in adj) {
+      for (let index3 in noun) {
+        const element1 = pronoun[index1];
+        const element2 = adj[index2];
+        const element3 = noun[index3];
+        aux +=
+          '<p><ul style="list-style: none;"><li>' +
+          element1 +
+          element2 +
+          element3 +
+          ".com" +
+          "</p></ul></li>";
+      }
+    }
+  }
+  document.write(aux);
 };
